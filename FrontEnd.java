9bdee05201c6d6390c44070fb819f49dfa287d8a
stonemoney <nickyg515@yahoo.com>
2021-04-26 15:20:15 -0400

    WIP

--- FrontEnd.java
+++ FrontEnd.java
@@ -15,6 +15,8 @@ import java.awt.*;
 
 import javax.swing.*;
 
+import java.awt.event.*;
+ 
 class FEPanel extends JPanel {
    public FEPanel(boolean editable) {
       add(new JLabel("Test!"));
@@ -22,8 +24,14 @@ class FEPanel extends JPanel {
 }
 
 public class FrontEnd extends JFrame {
-   public FrontEnd() {
+   DataLayer backend;
+   JComboBox searchFor;
+   JComboBox searchUsing;
+   JTextField searchBox;
+   JButton searchButton;
+   public FrontEnd(DataLayer backend) {
       super("University Database Controller");
+      this.backend = backend;
       JPanel mainLayoutPanel = new JPanel(new BorderLayout());
       // SIDEBAR
       JPanel databaseSelectorPanel = new JPanel();
@@ -38,9 +46,25 @@ public class FrontEnd extends JFrame {
       databaseSelectorPanel.add(interestsButton);
       mainLayoutPanel.add(databaseSelectorPanel, BorderLayout.EAST);
       // SEARCH BAR
-      JPanel searchBar = new JPanel(new FlowLayout());
-      JButton searchButton = new JButton("Search");
-      JTextField searchBox = new JTextField(40);
+      JPanel searchBar = new JPanel(new GridLayout(1,3));
+         //SEARCH SELECTOR
+         JPanel searchSelectors = new JPanel(new GridLayout(1,2));
+         JPanel searchForPanel = new JPanel(new BorderLayout());
+         JLabel searchForLabel = new JLabel("Search for:");
+         String[] searchForTerms = {"Faculty","Students","Interests","Degrees","Department"};
+         searchFor = new JComboBox(searchForTerms);
+         searchForPanel.add(searchForLabel, BorderLayout.NORTH);
+         searchForPanel.add(searchFor, BorderLayout.CENTER);
+         searchSelectors.add(searchForPanel);
+         JPanel searchUsingPanel = new JPanel(new BorderLayout());
+         JLabel searchUsingLabel = new JLabel("Using term:");
+         searchUsing = new JComboBox();
+         searchUsingPanel.add(searchUsingLabel, BorderLayout.NORTH);
+         searchUsingPanel.add(searchUsing, BorderLayout.CENTER);
+         searchSelectors.add(searchUsingPanel);
+      searchButton = new JButton("Search");
+      searchBox = new JTextField(40);
+      searchBar.add(searchSelectors);
       searchBar.add(searchBox);
       searchBar.add(searchButton);
       mainLayoutPanel.add(searchBar, BorderLayout.NORTH);
@@ -63,8 +87,51 @@ public class FrontEnd extends JFrame {
       setSize(500,600);
       setLocationRelativeTo(null);
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      //ACTIONS
+      searchFor.addActionListener(new ActionListener () {
+         public void actionPerformed(ActionEvent e) {
+            searchUsing.removeAllItems();
+            String[] usingTermsList = updateSearchUsing((String)searchFor.getSelectedItem());
+            for (String s : usingTermsList) {
+               searchUsing.addItem(s);
+            }
+         }
+      });
+      searchButton.addActionListener(new ActionListener () {
+         public void actionPerformed(ActionEvent e) {
+            search((String)searchBox.getText());  
+         }
+      });
    }
-   
+   public void search(String searchTerm) {
+      //TABLE, the table in the db to be looking through
+      String table = (String)searchFor.getSelected();
+      //TERM, the category/data field to be looking at
+      String term = (String)searchUsing.getSelected();
+      //SEARCH TERM, the actual text to search for
+      // (declaired as a param)
+      
+   }
+   public String[] updateSearchUsing(String selection) {
+      switch (selection) {
+         case "Faculty":
+            String[] terms = {"First Name","Last Name","Department","Email","Phone Number","Interests","Office Number","Abstracts"};
+            return terms;
+         case "Students":
+            String[] terms1 = {"First Name","Last Name","Interests","Degree","Email","Phone Number"};
+            return terms1;
+         case "Interests":
+            String[] terms2 = {"Description"};
+            return terms2;
+         case "Degrees":
+            String[] terms3 = {"Name"};
+            return terms3;
+         case "Department":
+            String[] terms4 = {"Name"};
+            return terms4;
+      }
+      return null;
+   }
    public static void main(String[] args) {
       FrontEnd fe = new FrontEnd();
    }
---
+++ Login.java
@@ -0,0 +1,130 @@
+/***
+ * Authors:
+ *      Farrell, Stephen
+ *      Giancursio, Nick
+ *      Gerber, Max
+ *      Gao, Fei
+ * Class: ISTE-330.02 DatabaseConnectivity and Access
+ * Date: 4/26/2021
+ * Professor: Habermas, Jim
+ * Assignment: ISTE-330.02 Group Project Group 4
+ * File: Login.java
+ */
+
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import javax.swing.*;
+
+public class Login extends JFrame implements ActionListener {
+
+    private JLabel labelUsername = new JLabel("Enter username: ");
+    private JLabel labelPassword = new JLabel("Enter password: ");
+    private JTextField textUsername = new JTextField(20);
+    private JPasswordField fieldPassword = new JPasswordField(20);
+    private JButton buttonLogin = new JButton("Login");
+    private JLabel labelMessage = new JLabel();
+
+
+    HashMap<String,String> logininfo = new HashMap<String, String>();
+
+    public Login(HashMap<String, String> loginInfoOriginal) {
+        super("RIT Faculty Abstract Project");
+
+        // Login hashmap
+        logininfo = loginInfoOriginal;
+        // create a new panel with GridBagLayout manager
+        JPanel newPanel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        // add components to the panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        newPanel.add(labelUsername, constraints);
+
+        constraints.gridx = 1;
+        newPanel.add(textUsername, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        newPanel.add(labelPassword, constraints);
+
+        constraints.gridx = 1;
+        newPanel.add(fieldPassword, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        newPanel.add(labelMessage, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        newPanel.add(buttonLogin, constraints);
+        buttonLogin.addActionListener(this);
+
+        // set border for the panel
+        newPanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Login Panel"));
+
+        // add the panel to this frame
+        add(newPanel);
+
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == buttonLogin) {
+            String userName = textUsername.getText();
+            String password = String.valueOf(fieldPassword.getPassword());
+
+            // If the username provided is contained in the hashmap
+            if (logininfo.containsKey(userName)) {
+                // If the password is equal to the password that maps to the username provided is correct
+                if (logininfo.get(userName).equals(password)) {
+                    labelMessage.setForeground(Color.green);
+                    labelMessage.setText("Login Successful");
+                    System.out.println("Login Successful");
+                    this.dispose();
+                    FrontEnd frontEnd = new FrontEnd();
+                }
+                // If Password is incorrect
+                else {
+                    labelMessage.setForeground(Color.red);
+                    labelMessage.setText("Invalid Password");
+                    System.out.println("Invalid Password");
+                }
+            } else {
+                labelMessage.setForeground(Color.red);
+                labelMessage.setText("Username not found!");
+                System.out.println("Username not found!");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        IDandPasswords iDandPasswords = new IDandPasswords();
+
+        // set look and feel to the system look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Login(iDandPasswords.getLoginInfo()).setVisible(true);
+            }
+        });
+    }
+}
+
